3cb09e25fd63197cb581c3e17f746e87d4f243fc
MaisonDeVolonte <11622069+MaisonDeVolonte@users.noreply.github.com>
2026-06-20 13:12:43 -0600

    color active tab icons and sort imports in Canvas.tsx
    - colored active tab icons using language-specific colors
    - grouped and ordered imports in Canvas.tsx according to project guidelines

--- src/modules/stage/Canvas.tsx
+++ src/modules/stage/Canvas.tsx
@@ -1,6 +1,8 @@
+import { ICON_COLORS, type ContentPage } from "@/cms/loader";
+
 import Link from "next/link";
 import Refractor from "@/modules/stage/Refractor";
-import type { ContentPage } from "@/cms/loader";
+
 import "@/modules/stage/Refractor.css";
 import "@/modules/stage/Canvas.css";
 
@@ -20,19 +22,25 @@ export default function Canvas({ page, activeIndex = 0 }: CanvasProps) {
   return (
     <div className="canvas">
       <div className="canvas__bar">
-        {page.files.map((file, i) => (
-          <Link
-            key={file.name}
-            href={tabHref(file.name)}
-            className={`canvas__tab${i === activeIndex ? " canvas__tab--active" : ""}`}
-          >
-            <span
-              className="canvas__tab-icon"
-              dangerouslySetInnerHTML={{ __html: file.icon }}
-            />
-            {file.name}
-          </Link>
-        ))}
+        {page.files.map((file, i) => {
+          const isActive = i === activeIndex;
+          const ext = file.iconName;
+          const iconColor = isActive ? ICON_COLORS[ext] : undefined;
+          return (
+            <Link
+              key={file.name}
+              href={tabHref(file.name)}
+              className={`canvas__tab${isActive ? " canvas__tab--active" : ""}`}
+            >
+              <span
+                className="canvas__tab-icon"
+                style={iconColor ? { color: iconColor } : undefined}
+                dangerouslySetInnerHTML={{ __html: file.icon }}
+              />
+              {file.name}
+            </Link>
+          );
+        })}
       </div>
       <Refractor code={active.content} language={active.language} />
     </div>
