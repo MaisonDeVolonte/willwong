2ce849c5a555e57db4dca6c2290dfea5608620e0
MaisonDeVolonte <11622069+MaisonDeVolonte@users.noreply.github.com>
2026-06-20 18:27:13 -0600

    misc: remove stray test-tree debug script
    - delete leftover root-level script that logged the clients nav node

--- test-tree.ts
+++
@@ -1,7 +0,0 @@
-import { getAllPages } from "./src/cms/loader";
-import { buildNavTree } from "./src/cms/tree";
-getAllPages().then(pages => {
-  const tree = buildNavTree(pages);
-  const clientsFolder = tree.find(node => node.label === "clients");
-  console.log("CLIENTS TREE NODE:", JSON.stringify(clientsFolder, null, 2));
-});
